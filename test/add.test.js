21903c4cc1c76e075cc5f761c691a9c24290134b
Kristjan Ulst <kristjan.ulst@arrive.com>
2026-04-25 00:03:46 +0300

    test: intentionally break test for A3 retry smoke

--- test/add.test.js
+++ test/add.test.js
@@ -3,6 +3,6 @@ import assert from "node:assert";
 import { add } from "../src/add.js";
 
 test("add returns the sum of two numbers", () => {
-  assert.strictEqual(add(2, 3), 5);
+  assert.strictEqual(add(2, 3), 999);
   assert.strictEqual(add(-1, 1), 0);
 });
